fb6b1b5a5f8f3860a57c425e71448324b7efe5db
Michael Grimes <michael.grimes@ou.edu>
2024-09-27 20:09:38 +0000

    added how to use the script

--- middle.sh
+++ middle.sh
@@ -1,5 +1,6 @@
 # script to return middle text of cubane file
 # created on September 27th, 2024
 # creator Michael Grimes
-
+# Usage:
+# include in first position, file path and name via command line
 head -n 9 "$1" | tail -n 3
